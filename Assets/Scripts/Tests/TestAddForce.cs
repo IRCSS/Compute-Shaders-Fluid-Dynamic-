4eb51e63ddf871cb583261d202240e1030ec4a12
Sha <sh.shahrabi@hotmail.com>
2020-10-17 17:13:17 +0200

    started adding the add force based on mouse pos kernel

---
+++ Assets/Scripts/Tests/TestAddForce.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class TestAddForce : MonoBehaviour
+{
+    // ------------------------------------------------------------------
+    // VARIABLES
+
+    //___________
+    // public
+    public  ComputeShader userInput_shader;
+    public  ComputeShader structuredBufferToTextureShader;
+
+    //___________
+    // private
+    private int           addForceHandle;
+    private int           st2txHandle;
+    private RenderTexture visulasation_texture;
+    private ComputeBuffer velocity_buffer;
+    private CommandBuffer cb;
+    private Camera        main_cam;
+    private int           canvas_dimension = 512;                         // Resolution of the render target used at the end, this can be lower or higher than the actual simulation grid resoltion
+    private int           i_Resolution     = 256;                         // Resolution of the simulation grid
+
+    void Start()
+    {
+        // -----------------------
+        main_cam = Camera.main;
+        if (main_cam == null) Debug.LogError("Could not find main camera, make sure the camera is tagged as main");
+        
+        main_cam.orthographic     = true;                                                                     // Make sure the camera is ortho. Perspecitve camera has a transformation matrix which will screw with everything
+        main_cam.aspect           = 1.0f;
+        main_cam.orthographicSize = 1;
+
+        // -----------------------
+        visulasation_texture = new RenderTexture(canvas_dimension, canvas_dimension, 0)
+        {
+            enableRandomWrite = true,
+            useMipMap         = false,
+        };
+
+
+        visulasation_texture.Create();
+        // -----------------------
+
+        velocity_buffer = new ComputeBuffer(i_Resolution * i_Resolution, sizeof(float) * 4);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Utility/ComputeShaderUtility.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public struct DispatchDimensions
+{
+    public uint dispatch_x;
+    public uint dispatch_y;
+    public uint dispatch_z;
+}
+
+public static class ComputeShaderUtility { 
+       static DispatchDimensions CheckGetDispatchDimensions(ComputeShader shader, int handle, uint desired_threadNum_x, uint desired_threadNum_y, uint desired_threadNum_z)
+       {
+       
+           uint group_size_x, group_size_y, group_size_z;
+       
+           shader.GetKernelThreadGroupSizes(handle, out group_size_x, out group_size_y, out group_size_z);
+       
+           if(desired_threadNum_x % group_size_x != 0 ||
+              desired_threadNum_y % group_size_y != 0 ||
+              desired_threadNum_z % group_size_z != 0)
+           {
+               Debug.LogError(string.Format("ERROR: Shader {0}, on kernel {1}, has mismatched desired thread numbers and thread groupd sizes. The desired thread "   +
+                                            "number needs to be a multiply of the group size. Either change the desired thread number or change the " +
+                                            "group size in the shader.", shader, handle));
+               Debug.Break();
+           }
+       
+       
+           DispatchDimensions dp;
+       
+           dp.dispatch_x = desired_threadNum_x / group_size_x;
+           dp.dispatch_y = desired_threadNum_y / group_size_y;
+           dp.dispatch_z = desired_threadNum_z / group_size_z;
+        uint totalThreadNumber = desired_threadNum_x * desired_threadNum_y * desired_threadNum_z;
+           if (totalThreadNumber < 64) Debug.LogWarning(string.Format("Total threads number is: {0}, this is probably too low and causes under utilization of GPU block!", totalThreadNumber));
+       
+           return dp;
+       }
+}
+
+
+
+
